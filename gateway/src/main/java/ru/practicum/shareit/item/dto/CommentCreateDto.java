66b48086ca0d231f169d1da34a2514df225626d7
Stanislav Emelianov <stasemel@yandex.ru>
2025-08-28 09:42:16 +0300

    Контроллер ItemController для gateway

---
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/CommentCreateDto.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.item.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+public class CommentCreateDto {
+    @NotNull
+    String text;
+}
---
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemCreateDto.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareit.item.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+public class ItemCreateDto {
+    @NotBlank(message = "Наименование должно быть указано")
+    String name;
+    @NotBlank(message = "Описание должно быть указано")
+    String description;
+    @NotNull(message = "Нужно указать, свободна или нет эта вещь")
+    Boolean available;
+    Long requestId;
+}
